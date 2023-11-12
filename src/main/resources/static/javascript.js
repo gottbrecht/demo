3846295b2d05b332c18113d879cf57b106dc5e99
gottbrecht <simo79j5@stud.kea.dk>
2023-11-12 13:09:44 +0100

    pom changes

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -1,34 +1,53 @@
-const SERVER_URL = 'http://localhost:8080/api';
+window.onload = function() {
+    const SERVER_URL = 'http://localhost:8080/api/song';
 
-document.getElementById('btn-get-song').addEventListener('click', getSong);
+    document.getElementById('btn-get-song').addEventListener('click', getSong);
 
-async function getSong() {
-    const topicInput = document.getElementById('topicInput').value.trim();
-    console.log('Topic Input:', topicInput);
+    async function getSong() {
+        const topicInput = document.getElementById('topicInput').value.trim();
+        console.log('Topic Input:', topicInput);
 
-    const songSpinner = document.getElementById('songSpinner');
-    const songResult = document.getElementById('songResult'); // Corrected variable name
-    songResult.style.color = 'black';
+        const songSpinner = document.getElementById('songSpinner');
+        const songOutput = document.getElementById('songOutput');
+        const counter = document.getElementById('counter');
 
-    try {
-        songSpinner.style.display = 'block';
+        songOutput.style.color = 'black';
 
-        const apiUrl = `${SERVER_URL}/song?topic=${encodeURIComponent(topicInput)}`; // Corrected string interpolation
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ topic: topicInput }),
-        }).then(response => response.json());
+        try {
+            songSpinner.style.display = 'block';
+            counter.innerText = '20';
 
-        songResult.innerText = `Recommended Song: ${response.answer}`; // Corrected string interpolation
-        document.getElementById('topicInput').value = '';
-    } catch (e) {
-        songResult.style.color = 'red';
-        songResult.innerText = `Error fetching song recommendation: ${e.message}`; // Corrected string interpolation
-    } finally {
-        songSpinner.style.display = 'none';
+            const apiUrl = `${SERVER_URL}?topic=${encodeURIComponent(topicInput)}`;
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({topic: topicInput}),
+            }).then(response => response.json());
+
+            songOutput.innerText = `Recommended Song: ${response.answer}`;
+            document.getElementById('topicInput').value = '';
+        } catch (e) {
+            songOutput.style.color = 'red';
+            songOutput.innerText = `Error fetching song recommendation: ${e.message}`;
+        } finally {
+            songSpinner.style.display = 'none';
+        }
     }
-}
 
+    let countdown = 20; // Initial countdown value
+    function startCountdown() {
+        countdown = 20;
+        const countdownInterval = setInterval(function () {
+            if (countdown > 0) {
+                countdown--;
+                document.getElementById('counter').innerText = countdown;
+            } else {
+                clearInterval(countdownInterval);
+            }
+        }, 1000);
+    }
+    document.getElementById('btn-get-song').addEventListener('click', startCountdown)
+
+};
