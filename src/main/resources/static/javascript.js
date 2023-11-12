5aa1a3974ea75bd2d678af11019d44909b2e2927
gottbrecht <simo79j5@stud.kea.dk>
2023-11-12 09:36:35 +0100

    pom changes

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -1,33 +1,34 @@
 const SERVER_URL = 'http://localhost:8080/api';
 
 document.getElementById('btn-get-song').addEventListener('click', getSong);
-async function getSong(event) {
-    event.preventDefault();
+
+async function getSong() {
     const topicInput = document.getElementById('topicInput').value.trim();
     console.log('Topic Input:', topicInput);
+
     const songSpinner = document.getElementById('songSpinner');
-    const songResult = document.getElementById('songResult');
+    const songResult = document.getElementById('songResult'); // Corrected variable name
     songResult.style.color = 'black';
 
     try {
         songSpinner.style.display = 'block';
-        const apiURL = '${SERVER_URL}/song?topic=${encodeURIComponent(topicInput)}';
-        const response = await fetch(apiUrl).then(handleHttpErrors);
-        songResult.innerText = 'Recommended Song: ${response.song}';
+
+        const apiUrl = `${SERVER_URL}/song?topic=${encodeURIComponent(topicInput)}`; // Corrected string interpolation
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ topic: topicInput }),
+        }).then(response => response.json());
+
+        songResult.innerText = `Recommended Song: ${response.answer}`; // Corrected string interpolation
         document.getElementById('topicInput').value = '';
     } catch (e) {
         songResult.style.color = 'red';
-        songResult.innerText = 'Error fetcing song recommendation: ${e.message}';
+        songResult.innerText = `Error fetching song recommendation: ${e.message}`; // Corrected string interpolation
     } finally {
         songSpinner.style.display = 'none';
     }
-}
-
-
-function handleHttpErrors(response) {
-    if(!response.ok) {
-        throw new Error('HTTP error! Status: ${response.status}');
-    }
-    return response.json();
 }
 
